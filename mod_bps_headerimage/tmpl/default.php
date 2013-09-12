34a916870d9a9e1e17e33ebe9d8a99913df60b03
Matthijs Alles <admin@bixie.nl>
2013-09-12 19:44:26 +0200

    aanpassingen na test

--- mod_bps_headerimage/tmpl/default.php
+++ mod_bps_headerimage/tmpl/default.php
@@ -9,7 +9,13 @@
 // no direct access
 defined('_JEXEC') or die;
 ?>
-<div id="headerimage" style="background-image: url('<?php echo $sImagepath; ?>');max-width:<?php echo $iWidth; ?>px;max-height:<?php echo $iHeight; ?>px;">
-<div class="tekstregel1"><?php echo $sTekstregel1; ?></div>
-<div class="tekstregel2"><?php echo $sTekstregel2; ?></div>
+<div id="headerimage" class="widthmax" style="background-image: url('<?php echo $sImagepath; ?>');max-width:<?php echo $iWidth; ?>px;max-height:<?php echo $iHeight; ?>px;">
+	<div class="wrapper">
+		<?php if ($sTekstregel1 != '') : ?>
+			<div class="tekst tekstregel1"><?php echo $sTekstregel1; ?></div>
+		<?php endif; ?>
+		<?php if ($sTekstregel2 != '') : ?>
+			<div class="tekst tekstregel2"><?php echo $sTekstregel2; ?></div>
+		<?php endif; ?>
+	</div>
 </div>
