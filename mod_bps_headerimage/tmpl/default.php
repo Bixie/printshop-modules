ed7deac0e361c39dc55a45f6ad3547f145cd55ad
Matthijs Alles <admin@bixie.nl>
2013-09-12 18:27:00 +0200

    productafbeelding herkenning

--- mod_bps_headerimage/tmpl/default.php
+++ mod_bps_headerimage/tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *	com_bix_printshop - Online-PrintStore for Joomla
- *  Copyright (C) 2010-2012 Matthijs Alles
+ *  Copyright (C) 2010-2013 Matthijs Alles
  *	Bixie.nl
  *
  */
@@ -9,14 +9,7 @@
 // no direct access
 defined('_JEXEC') or die;
 ?>
-<div class="grid-block width100 aanbiedingtekst">
-	<div class="bg width100" style="background:url('/<?php echo $image; ?>') no-repeat scroll 100% 0 transparent;min-height:<?php echo $height; ?>px;">
-		<div class="tekst ">
-			<?php echo $tekst; ?>
-		</div>
-	</div>
-</div>
-<div class="grid-block width100 modfooter">
-	<?php echo JText::_('MOD_BPS_AANBIEDING_PRIJSINFO'); ?>
-	<a href="<?php echo $item->prodLink; ?>" class="button"><span class="buttontext"><?php echo JText::_('MOD_BPS_AANBIEDING_BESTELLEN'); ?></span></a>
+<div id="headerimage" style="background-image: url('<?php echo $sImagepath; ?>');max-width:<?php echo $iWidth; ?>px;max-height:<?php echo $iHeight; ?>px;">
+<div class="tekstregel1"><?php echo $sTekstregel1; ?></div>
+<div class="tekstregel2"><?php echo $sTekstregel2; ?></div>
 </div>
