ba8e6783d9208adcac7061e5af645d07704891fd
Matthijs Alles <admin@bixie.nl>
2013-09-17 20:37:39 +0200

    productcart prijs optioneel

--- mod_bps_cart/tmpl/product_raw.php
+++ mod_bps_cart/tmpl/product_raw.php
@@ -10,12 +10,15 @@
 defined('_JEXEC') or die;
 ?>
 
-<?php  require JModuleHelper::getLayoutPath('mod_bps_cart', 'default_raw'); ?>
-	<?php if ($bixCart->get('nrOrders',0) > 0) :?>
-	<div class="grid-block">
-		<a class="tocartlink float-left" href="<?php echo JRoute::_('index.php?Itemid='.BixTools::config('algemeen.cartItemid'))?>"><?php echo JText::_('MOD_BPS_TOCARTLINK'); ?></a>
+	<div class="cartItems">
+		<?php  require JModuleHelper::getLayoutPath('mod_bps_cart', 'default_raw'); ?>
+		<?php if ($bixCart->get('nrOrders',0) > 0) :?>
+		<div class="grid-block">
+			<a class="tocartlink float-left" href="<?php echo JRoute::_('index.php?Itemid='.BixTools::config('algemeen.cartItemid'))?>"><?php echo JText::_('MOD_BPS_TOCARTLINK'); ?></a>
+		</div>
 	</div>
 	<?php endif; ?>
+	<?php if ($params->get('showPrices',0) > 0) :?>
 	<div id="productPriceHolder">
 		<div class="prijshold totaalText"><?php echo JText::_('Totaal bedrag:'); ?></div>
 		<div class="prijshold origHolder"><?php echo JText::_('Adviesprijs '); ?><div class="strike"><div class="origPrijs"></div></div></div>
@@ -24,6 +27,7 @@ defined('_JEXEC') or die;
 		<div class="prijshold brutoHolder">(<div class="orderBruto"></div> <?php echo JText::_('MOD_BPS_CART_INCLBTW'); ?>)</div>
 	</div>
 	<button type="button" class="button" id="orderButton"><?php echo JText::_('Bestellen'); ?></button>
+	<?php endif; ?>
 
 	<ul id="cartInfoUL" class="rawlist"> 
 	</ul>
