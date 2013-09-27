c7b6b836e6ac7056f2bf00c7b744c050a84a7254
Matthijs Alles <admin@bixie.nl>
2013-09-27 20:20:00 +0200

    rich snippets

---
+++ mod_feedback/tmpl/dhnsmall.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_feed
+ * @copyright	Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+?>
+<div class="fb-small" itemscope itemtype="http://data-vocabulary.org/Review-aggregate">
+<!--<div class="fb-title">Reviews (<? echo $reviews; ?>)</div>-->
+	<span class="hidden" itemprop="itemreviewed">Drukhetnu.nl</span>
+	<span itemprop="rating" itemscope itemtype="http://data-vocabulary.org/Rating">
+		<span class="fb-rating" itemprop="average"><? echo $score; ?></span> / <span class="fb-scale" itemprop="best"><? echo $scoremax; ?></span><br/>
+	</span>
+	<div class="rstar rstar-<? echo number_format(modFeedbackHelper::round_to_half($score),1,'',''); ?>"></div>
+	<br/><span class="fb-count" itemprop="votes"><? echo $reviews; ?></span> reviews
+</div>
+
