a059be9a7a0aba591c7e0e0f90e1309d77eead63
Matthijs Alles <admin@bixie.nl>
2014-01-12 23:43:58 +0100

    install init

---
+++ administrator/mod_bps_orders/install.script.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *	com_bix_printshop - Online-PrintStore for Joomla
+ *  Copyright (C) 2014 Matthijs Alles
+ *	Bixie.org
+ *
+ */
+ 
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+class modBps_ordersInstallerScript {
+
+	protected $_ext = 'mod_bps_orders';
+
+	/**
+	 * Called on installation
+	 *
+	 * @param   object  $parent  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	function install($parent)
+	{
+		// init vars
+		$db = JFactory::getDBO();
+
+		}
+
+
+
+
+}
